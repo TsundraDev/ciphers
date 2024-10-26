80bd5a161e931b0b196cd71ad18c9e37d96f64a8
Tsundra <tsundradev@gmail.com>
2024-10-26 17:33:08 +0700

    Added cmd line support

--- cipher.py
+++ cipher.py
@@ -1,3 +1,5 @@
+import sys
+
 def caesar_cipher(key, plaintext):
     for i, c in enumerate(plaintext):
         # Turn upper character to lower
@@ -6,26 +8,31 @@ def caesar_cipher(key, plaintext):
         # Modify if alphabetical
         if ('a' <= c and c <= 'z'):
             c_num = ord(c) + key
-            if (c_num > ord('z')):
+            while (c_num > ord('z')):
                 c_num -= 26
             c = chr(c_num)
             plaintext = plaintext[:i] + c + plaintext[i+1:]
         # Modify if numerical
         elif ('0' <= c and c <= '9'):
             c_num = ord(c) + key
-            if (c_num > ord('9')):
+            while (c_num > ord('9')):
                 c_num -= 10
             c = chr(c_num)
             plaintext = plaintext[:i] + c + plaintext[i+1:]
     return plaintext
 
+# Check cmd-line arguments
+if (len(sys.argv) != 4):
+    print("Usage: python3 cipher.py [key] [plaintext-file] [ciphertext-file]")
+key = ord(sys.argv[1]) - ord('A')
+plaintext_filename  = sys.argv[2]
+ciphertext_filename = sys.argv[3]
+
 # Prepare ciphertext
-key = ord('E') - ord('A')
-with open("text.plain", 'r') as f:
+with open(plaintext_filename, 'r') as f:
     plaintext  = f.read()
 ciphertext = caesar_cipher(key, plaintext)
 
-
 # Write text to file
-with open("text.cipher", 'w') as f:
+with open(ciphertext_filename, 'w') as f:
     f.write(ciphertext)
